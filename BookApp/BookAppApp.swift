26f1e332d9f15b6fce43d8c5b8832d77270f83e5
hampus-code <hampusss96@gmail.com>
2024-10-10 17:15:55 +0200

    First commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Hampus Andersson on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
